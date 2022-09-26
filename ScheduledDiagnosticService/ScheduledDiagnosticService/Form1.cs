874e98c2a7b1a961ed3233810d49c0835ee909d8
Александр Л. Шарапов <sharapov@office.local>
2022-09-26 15:11:27 +0300

    Установить общую дату и время диагностирования для всех алгоритмов
     - испарвлено

--- ScheduledDiagnosticService/ScheduledDiagnosticService/Form1.cs
+++ ScheduledDiagnosticService/ScheduledDiagnosticService/Form1.cs
@@ -54,7 +54,66 @@ namespace ScheduledDiagnosticService
 
         private async void timer1_Tick(object sender, EventArgs e)
         {
-            //
+            try
+            {
+                var builder = new ConfigurationBuilder();
+                // установка пути к текущему каталогу
+                builder.SetBasePath(Directory.GetCurrentDirectory());
+                // получаем конфигурацию из файла appsettings.json
+                builder.AddJsonFile("appsettings.json");
+                // создаем конфигурацию
+                var config = builder.Build();
+                // получаем строку подключения
+                string connectionString = config.GetConnectionString("DiagServiceConnection");
+                using (DiagServiceContext db = new DiagServiceContext(connectionString))
+                {
+                    var _sections = db.Sections.ToList();
+                    string _s_sectionId = "";
+                    string _s_algoritms = "";
+
+                    foreach (Section s in _sections)
+                    {
+                        //outToLog("\r\n" + s.Notation, Color.Black);
+                        _s_algoritms = "";
+                        foreach (TimeTable tt in s.TimeTables)
+                        {
+                            _s_algoritms += tt.Algoritm?.Notation;
+                        }
+                        _s_algoritms = _s_algoritms.Replace("**", "*");
+                        //logRichTextBox.AppendText(_s_algoritms);
+                        if (s.RefID.HasValue)
+                        {
+                            _s_sectionId = ((int)s.RefID).ToString();
+                        }
+
+                        try
+                        {
+                            // 1) по таймеру (раз в сутки) считать из БД список секций лдокомотивов и алгоритмов диагностирования для них;
+                            // 2) для каждой скекции из списка, считанных из БД, запустить диагностирование;
+                            // 3) результаты необходимо записать в БД для последующего использования через вызов API;
+
+                            outToLog("Запуск алгоритмов диагностики по расписанию: " + s.Notation + " (" + DateTime.Now.ToString() + ")", Color.Black);
+                            Diagnostic DIA = new Diagnostic(System.Configuration.ConfigurationManager.ConnectionStrings["lcmConnection"].ConnectionString, _s_sectionId, _s_algoritms, DateTime.Now.AddMonths(-1).ToString("yyyy.MM.dd"), DateTime.Now.ToString("yyyy.MM.dd")); //объект класса диагностики   
+                            DIA.Notify += outToLog;
+                            Report_Diagnostic_Models rezultDiagnostic = await DIA.GetDiagnosticAsync(); // rezultDiagnostic = await Task.Run(() => DIA.GetDiagnostic());                            
+                            Result _result = await new SaveDiagnosticResult().SaveResultDiagnosticAsync(rezultDiagnostic, connectionString, Convert.ToInt32(_s_sectionId));
+                            //outToLog(DIA.Seriya + DIA.Number + DIA.Section + " Done: " + DateTime.Now.ToString(), Color.Green);
+                        }
+                        catch (System.Exception ex)
+                        {
+                            outToLog(ex.Message, Color.Red);
+                        }
+                        finally
+                        {
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                outToLog(ex.Message, Color.Red);
+            }
         }
         /// <summary>
         /// Ввод только цифр в поле периода диагностирования
@@ -155,7 +214,6 @@ namespace ScheduledDiagnosticService
             {
                 outToLog(ex.Message, Color.Red);
             }
-
         }
     }
 }
